623685f6b14abf4903d087593b168add8c32f937
seifbenothman <seifbenothman95@gmail.com>
2024-04-24 07:22:54 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,10 +1,9 @@
 function getResponseFromAPI() {
 return new Promise((resolve, reject) => {
 	setTimeout(() => {
-		const responseData = { message: "Dummy response from API"};
-			resolve(responseData);
+	const responseData = { message: "Dummy response from API"};
+		resolve(responseData);
 	}, 1000);
 });
 }
-
 export default getResponseFromAPI;
