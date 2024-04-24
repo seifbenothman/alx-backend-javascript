4e52f8f2b07f2b80921e87390cbb8e1193c3fcf0
seifbenothman <seifbenothman95@gmail.com>
2024-04-24 07:26:32 +0000

    0-promise.js

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,7 +1,3 @@
-function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve("Response from API");
-  });
+export default function getResponseFromAPI() {
+  return new Promise(() => {});
 }
-
-export default getResponseFromAPI;
