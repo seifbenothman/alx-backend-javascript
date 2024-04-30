4d825a97fdf44e7fb8c69f73b902ea62ee57e341
seifbenothman <seifbenothman95@gmail.com>
2024-04-30 20:31:50 +0000

    100-weak.js

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -2,19 +2,14 @@ const weakMap = new WeakMap();
 
 export { weakMap };
 
-function queryAPI(endpoint) {
-  if (!weakMap.has(endpoint)) {
-    weakMap.set(endpoint, 0);
+export function queryAPI(endpoint) {
+  if (weakMap.has(endpoint)) {
+    weakMap.set(endpoint, weakMap.get(endpoint) + 1);
+  } else {
+    weakMap.set(endpoint, 1);
   }
-
-  const count = weakMap.get(endpoint) + 1;
-  weakMap.set(endpoint, count);
-
-  if (count >= 5) {
+  
+  if (weakMap.get(endpoint) >= 5) {
     throw new Error('Endpoint load is high');
   }
-
-  return count;
 }
-
-export default queryAPI;
